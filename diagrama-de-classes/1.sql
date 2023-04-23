d0fb1819858710ef14b4f55f502225448f784ea1
Pablo Busatto <55034604+PabloBF@users.noreply.github.com>
2023-04-23 20:51:20 -0300

    Create 1.sql

---
+++ diagrama-de-classes/1.sql
@@ -0,0 +1,26 @@
+CREATE TABLE Funcionario (
+	id SERIAL PRIMARY KEY,
+	nome VARCHAR(50) NOT NULL,
+	sobrenome VARCHAR(50) NOT NULL,
+    nome_social VARCHAR(150),
+    cpf CHAR(11),
+    data_nascimento Date,
+    endereco VARCHAR(255),
+    telefone VARCHAR(15),
+    email VARCHAR(50),
+    email_corporativo VARCHAR(50),
+    cargo INTEGER,
+    senha VARCHAR(64) PASSWORD,
+    data_registro TIMESTAMP,
+    situacao INTEGER
+)
+
+CREATE TABLE Situacao (
+	id SERIAL PRIMARY KEY,
+	situacao VARCHAR(50)
+)
+
+CREATE TABLE Cargo (
+	id SERIAL PRIMARY KEY,
+	cargo VARCHAR(50)
+)
