365cefad956671e2cd82857729db2baae935026e
Pablo Busatto <55034604+PabloBF@users.noreply.github.com>
2023-04-26 08:54:32 -0300

    Update 1.sql
    tabulação

--- diagrama-de-classes/1.sql
+++ diagrama-de-classes/1.sql
@@ -2,17 +2,17 @@ CREATE TABLE Funcionario (
 	id SERIAL PRIMARY KEY,
 	nome VARCHAR(50) NOT NULL,
 	sobrenome VARCHAR(50) NOT NULL,
-    nome_social VARCHAR(150),
-    cpf CHAR(11),
-    data_nascimento Date,
-    endereco VARCHAR(255),
-    telefone VARCHAR(15),
-    email VARCHAR(50),
-    email_corporativo VARCHAR(50),
-    cargo INTEGER,
-    senha VARCHAR(64) PASSWORD,
-    data_registro TIMESTAMP,
-    situacao INTEGER
+	nome_social VARCHAR(150),
+	cpf CHAR(11),
+	data_nascimento Date,
+	endereco VARCHAR(255),
+	telefone VARCHAR(15),
+	email VARCHAR(50),
+	email_corporativo VARCHAR(50),
+	cargo INTEGER,
+	senha VARCHAR(64) PASSWORD,
+	data_registro TIMESTAMP,
+	situacao INTEGER
 )
 
 CREATE TABLE Situacao (
